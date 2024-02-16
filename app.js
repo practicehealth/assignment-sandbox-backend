fa3dc102e5e217e356336c3e570af2313878ee4d
aditya-kumarr <127249885+aditya-kumarr@users.noreply.github.com>
2024-02-16 19:00:15 +0530

    set cors origin to wildcard

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(loggerMiddleware);
 app.get("/", (_, res) => {
   res.send(
